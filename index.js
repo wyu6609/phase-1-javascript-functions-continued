3d4feff2ec3b944a75f8fa289a0402f34f3f3e8a
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-01-29 20:20:18 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(argument = "roller-skate") {
+  return `This Saturday, I want to ${argument}!`;
+}
+function mondayWork(argument = "go to the office") {
+  return `This Monday, I will ${argument}.`;
+}
+function wrapAdjective(string = "*") {
+  return function (argument = "special") {
+    return `You are ${string}${argument}${string}!`;
+  };
+}
